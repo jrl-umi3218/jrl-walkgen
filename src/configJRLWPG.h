9b51caa4d4e4774f58f4d022d1db1a85ae66caba
Olivier Stasse <stasse@dhcpt1059.a02.aist.go.jp>
2007-01-17 21:08:00 +0000

    OS: configJRLWPG.h embed all the options related to matrices. The documentation for this is not yet available.

---
+++ src/configJRLWPG.h
@@ -0,0 +1,10 @@
+
+/*! Uses the default implementation for the small 4x4 matrices */
+#define _DEFAULT_MATRIX4x4_ 1
+
+/*! Uses the default implementation for the small 3x3 matrices */
+#define _DEFAULT_MATRIX3x3_ 1
+
+/*! Uses the default implementation for the small 3d vector */
+#define _DEFAULT_VECTOR3D_ 1
+
