33efb59ce97f7ea501453c96cf77ef6770b8ed06
Olivier Stasse <stasse@dhcpt1243.a02.aist.go.jp>
2008-11-06 14:06:00 +0900

    Clean up the installation process.

--- walkGenJrl.pc.cmake
+++ walkGenJrl.pc.cmake
@@ -9,5 +9,5 @@ Name: ${PROJECT_NAME}
 Description: ${PROJECT_DESCRIPTION}
 Version: ${PROJECT_VERSION}
 Requires: ${PROJECT_REQUIREMENTS}
-Libs:  ${${PROJECT_NAME}_LDFLAGS}
+Libs:  -L${install_pkg_libdir} ${${PROJECT_NAME}_LDFLAGS}
 Cflags: -I${install_pkg_include_dir} ${${PROJECT_NAME}_CXXFLAGS}
