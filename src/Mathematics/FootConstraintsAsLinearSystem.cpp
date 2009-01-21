0e74af527db0090b224546528a4f1eaa091d1900
Francois Keith <francois.keith@aist.go.jp>
2009-01-21 14:18:45 +0100

    Win32 compatibility : - add the \#include for M_PI - correct the documentation computation command

--- src/Mathematics/FootConstraintsAsLinearSystem.cpp
+++ src/Mathematics/FootConstraintsAsLinearSystem.cpp
@@ -32,6 +32,8 @@
 #include <iostream>
 #include <fstream>
 
+#include <jrlMathTools/jrlConstants.h>
+
 #include <dynamicsJRLJapan/HumanoidSpecificities.h>
 
 #include <walkGenJrl/Mathematics/FootConstraintsAsLinearSystem.h>
