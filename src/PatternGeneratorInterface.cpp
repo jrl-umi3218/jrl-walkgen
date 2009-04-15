9fd0f905955fbf0609c830d819efe9610341d7f1
stasse <olivier.stasse@aist.go.jp>
2009-04-15 09:51:24 +0900

    [src] Make sure that the code is still compatiable with dynamicJRLJapan master.
    Removed the call to AngularMomentum.
    WARNING: dynamicsJRLJapan is going under some modifications.
    A Buildbot campaign is launched to test the potential
    broken packages. (Should be quite mimimal).

--- src/PatternGeneratorInterface.cpp
+++ src/PatternGeneratorInterface.cpp
@@ -91,7 +91,7 @@ namespace PatternGeneratorJRL {
     RESETDEBUG4("DebugDataIKArms.txt");
     RESETDEBUG4("DebugDataOnLine.txt");
     RESETDEBUG4("DebugDataWaistYaw.dat");
-    RESETDEBUG5("DebugDataAngularMomentum.dat");
+    RESETDEBUG4("DebugDataAngularMomentum.dat");
 
     RESETDEBUG4("UpperBodyAngles.txt");
     RESETDEBUG4("DebugZMPFinale.txt");
@@ -1604,8 +1604,8 @@ namespace PatternGeneratorJRL {
     _2DMBCoM= m_2HumanoidDynamicRobot->getPositionCoM();
 
     vector3d lAngularMomentum;
-    lAngularMomentum = m_2HumanoidDynamicRobot->angularMomentumWrtCoM();
-    ODEBUG5(m_count << " " << lAngularMomentum(0)
+    //    lAngularMomentum = m_2HumanoidDynamicRobot->angularMomentumWrtCoM();
+    ODEBUG4(m_count << " " << lAngularMomentum(0)
 	    << " " << lAngularMomentum(1)
 	    << " " << lAngularMomentum(2), "DebugDataAngularMomentum.dat");
 
