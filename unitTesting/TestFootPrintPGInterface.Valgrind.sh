42835c90c204ce921eb4fd04e96c001dc3cbafeb
stasse <olivier.stasse@aist.go.jp>
2009-06-03 06:49:08 +0900

    [unitTesting] Adding script to perform valgrind check.
    	* unitTesting/TestFootPrintPGInterface.Valgrind.sh:
    	New file - this version has the full options.
    
    Signed-off-by: stasse <olivier.stasse@aist.go.jp>

---
+++ unitTesting/TestFootPrintPGInterface.Valgrind.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+export HRP2JRLDIR=${HOME}/src/OpenHRP/Controller/IOserver/robot/HRP2JRL
+export ETCDIR=${HRP2JRLDIR}/etc
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${PWD}/../src/
+echo $LD_LIBRARY_PATH
+valgrind --leak-check=full --error-limit=no -v -v ./TestFootPrintPGInterface ${ETCDIR}/PreviewControlParameters.ini ${HRP2JRLDIR}/model/ HRP2JRLmain.wrl ${ETCDIR}/HRP2Specificities.xml ${ETCDIR}/HRP2LinkJointRank.xml &> output_valgrind.dat
