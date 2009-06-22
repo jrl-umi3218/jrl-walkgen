5918872465a08641de4625f8f26545ebe5ccecb9
Olivier Stasse <olivier.stasse@aist.go.jp>
2009-06-22 11:17:05 +0900

    Reactivated test for optimal cholesky implementation.
    	* unitTesting/CMakeLists.txt:
    	Added the file in the list of test to compile.
    	* unitTesting/TestOptCholesky.cpp:
    	Modification to comply with the new interface to be
    	tested.
    
    Signed-off-by: Olivier Stasse <olivier.stasse@aist.go.jp>

--- unitTesting/TestOptCholesky.cpp
+++ unitTesting/TestOptCholesky.cpp
@@ -11,26 +11,7 @@
 
    All rights reserved.
    
-   Redistribution and use in source and binary forms, with or without modification, 
-   are permitted provided that the following conditions are met:
-   
-   * Redistributions of source code must retain the above copyright notice, 
-   this list of conditions and the following disclaimer.
-   * Redistributions in binary form must reproduce the above copyright notice, 
-   this list of conditions and the following disclaimer in the documentation 
-   and/or other materials provided with the distribution.
-   * Neither the name of the CNRS/AIST nor the names of its contributors 
-   may be used to endorse or promote products derived from this software without specific prior written permission.
-   
-   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS 
-   OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY 
-   AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER 
-   OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, 
-   OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS 
-   OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) 
-   HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, 
-   STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING 
-   IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+   Please read License.txt for more information on the license.
 
 */
 
@@ -112,7 +93,8 @@ int main()
   double * iL = new double[lNbOfConstraints*lNbOfConstraints];
 
   /* Create the object for optimized Cholesky computation */
-  anOptCholesky = new PatternGeneratorJRL::OptCholesky(lNbOfConstraints,lCardU);
+  anOptCholesky = new PatternGeneratorJRL::OptCholesky(lNbOfConstraints,lCardU,
+						       PatternGeneratorJRL::OptCholesky::MODE_NORMAL);
 
   /* Build the test matrix */
   srand(0);
@@ -126,7 +108,8 @@ int main()
   if (verbose>1)
     DisplayMatrix(A,lNbOfConstraints,lCardU,string("A"),0);
 
-  anOptCholesky->SetA(A);
+  anOptCholesky->SetA(A,
+		      PatternGeneratorJRL::OptCholesky::MODE_NORMAL);
   anOptCholesky->SetL(L);
   anOptCholesky->SetiL(iL);
 
@@ -158,13 +141,14 @@ int main()
   delete anOptCholesky;
 
   /* Create the object for normal Cholesky computation */
-  anOptCholesky = new PatternGeneratorJRL::OptCholesky(lNbOfConstraints,lNbOfConstraints);
+  anOptCholesky = new PatternGeneratorJRL::OptCholesky(lNbOfConstraints,lNbOfConstraints,
+						       PatternGeneratorJRL::OptCholesky::MODE_NORMAL);
 
-  anOptCholesky->SetA(AAT);
+  anOptCholesky->SetA(AAT,PatternGeneratorJRL::OptCholesky::MODE_NORMAL);
   anOptCholesky->SetL(L);
   anOptCholesky->SetiL(iL);
 
-  anOptCholesky->ComputeNormalCholeskyOnA();
+  anOptCholesky->ComputeNormalCholeskyOnANormal();
 
   r = CheckCholeskyDecomposition(AAT,L,lNbOfConstraints);
   if (verbose>1)
@@ -179,7 +163,7 @@ int main()
     }
   
 
-  anOptCholesky->ComputeInverseCholesky(1);
+  anOptCholesky->ComputeInverseCholeskyNormal(1);
   if (verbose>1)
     DisplayMatrix(iL,lNbOfConstraints,lNbOfConstraints,string("iL"),0);
 
