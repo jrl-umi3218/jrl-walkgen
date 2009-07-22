9de3c95f5e8d196d35522f096a374f8879247b67
Olivier Stasse <olivier.stasse@aist.go.jp>
2009-07-22 12:52:01 +0900

    Now WalkGenJrl.cpp do not support OpenHRP 2.0 anymore.

--- plugin/WalkGenJRL.cpp
+++ plugin/WalkGenJRL.cpp
@@ -7,26 +7,8 @@
     JRL-Japan, CNRS/AIST
 
     All rights reserved.
-   
-    Redistribution and use in source and binary forms, with or without modification, 
-    are permitted provided that the following conditions are met:
-   
-    * Redistributions of source code must retain the above copyright notice, 
-    this list of conditions and the following disclaimer.
-    * Redistributions in binary form must reproduce the above copyright notice, 
-    this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
-    * Neither the name of the CNRS/AIST nor the names of its contributors 
-    may be used to endorse or promote products derived from this software without specific prior written permission.
-   
-    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS 
-    OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY 
-    AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER 
-    OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, 
-    OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS 
-    OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) 
-    HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, 
-    STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING 
-    IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+   Please look at License.txt for details on the license.   
 */
 
 #if 0 /* MAL 2.0 */
@@ -37,21 +19,7 @@ namespace ml = maal::boost;
 #endif
 
 
-#ifdef OPENHRP_VERSION_2
-#include "plugin.h"
-#include "dynamicsPlugin.h"
-#include "seqplugin.h"
 
-typedef robot_state sotRobotState;
-typedef motor_command sotMotorCommand;
-typedef seqplugin_ptr sotSequencePlayer_var;
-typedef seqplugin sotSequencePlayer;
-
-#define ROBOT_STATE_VAR_POS(x) x->waistPos
-#define ROBOT_STATE_VAR_ATT(x) x->waistRpy
-#endif
-
-#ifdef OPENHRP_VERSION_3
 
 #include "Plugin_impl.h"
 #include "SequencePlayer.h"
@@ -69,13 +37,9 @@ typedef SequencePlayer sotSequencePlayer;
 #define ROBOT_STATE_VAR_POS(x) x->basePos
 #define ROBOT_STATE_VAR_ATT(x) x->baseAtt
 
-#endif
 
 #include "bodyinfo.h"	    
 
-#ifdef ORBIX
-#include <walkpluginJRL_skel.h>
-#endif
 #ifdef OMNIORB4
 #include <walkpluginJRL.h>
 #endif
