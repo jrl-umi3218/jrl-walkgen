6f48c5544b1b43bc629a702202835f9fe6cc1a57
Herdt Andrei <andrei.herdt@gmail.com>
2010-06-04 14:49:39 +0900

    changed the size of the simple support areas to 60*120 mm

--- src/Mathematics/footConstraintsAsLinearSystem.cpp
+++ src/Mathematics/footConstraintsAsLinearSystem.cpp
@@ -101,11 +101,11 @@ footConstraintsAsLinearSystem::footConstraintsAsLinearSystem(SimplePluginManager
   // lRightFootHalfWidth -= ConstraintOnX;
 
   //Andremize: Scilab feet
-  lLeftFootHalfHeight = 0.069-0.02;
-  lRightFootHalfHeight = 0.069-0.02;
+  lLeftFootHalfHeight = 0.069-0.039;
+  lRightFootHalfHeight = 0.069-0.039;
 
-  lLeftFootHalfWidth = 0.1206-0.02;
-  lRightFootHalfWidth = 0.1206-0.02;
+  lLeftFootHalfWidth = 0.1206-0.0606;
+  lRightFootHalfWidth = 0.1206-0.0606;
 
   DSFeetDistance = 0.2;
   lLeftFootHalfHeightDS = lLeftFootHalfHeight+DSFeetDistance/2.0;
