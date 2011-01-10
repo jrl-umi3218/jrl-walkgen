4c56fb1c1e92b7f409a4b1a8f04448f4a9942163
Andrei <andrei.herdt@inrialpes.fr>
2011-01-10 09:19:30 +0100

    Add members and methods
    - Add struct dynamics
    - Add members for dynamics and linear inequalities
    - Add accessors
    - Add selection matrices
    - Add operator and method for dumping

--- src/Mathematics/intermediate-qp-matrices.hh
+++ src/Mathematics/intermediate-qp-matrices.hh
@@ -1,8 +1,8 @@
 /*
- * Copyright 2010,
+ * Copyright 2011,
  *
- * Andrei   Herdt
  * Olivier  Stasse
+ * Andrei   Herdt
  *
  * JRL, CNRS/AIST
  *
@@ -22,23 +22,21 @@
  *  Research carried out within the scope of the
  *  Joint Japanese-French Robotics Laboratory (JRL)
  */
-/*! This object provides intermediate elements and operations that are necessary to construct a QP.
+/*! Custom (value based) container providing intermediate elements for the construction of a QP.
  */
 
 #ifndef INTERMEDQPMAT_HH_
 #define INTERMEDQPMAT_HH_
 
-//# include <boost/numeric/ublas/matrix.hpp>
-//# include <boost/numeric/ublas/vector.hpp>
 #include <jrl/mal/matrixabstractlayer.hh>
 #include <privatepgtypes.h>
 
 #include <iostream>
 #include <fstream>
 
+
 namespace PatternGeneratorJRL
 {
-  //namespace ublas = boost::numeric::ublas; 
 
   class  IntermedQPMat
   {
@@ -50,7 +48,31 @@ namespace PatternGeneratorJRL
     const static int MEAN_VELOCITY = 0;
     const static int INSTANT_VELOCITY = 1;
     const static int COP_CENTERING = 2;
-    const static int JERK = 3;
+    const static int JERK_MIN = 3;
+
+    const static int POSITION = 10;
+    const static int VELOCITY = 11;
+    const static int ACCELERATION = 12;
+    const static int JERK = 13;
+    const static int COP = 14;
+
+    const static int INEQ_COP = 20;
+    const static int INEQ_COM = 21;
+    const static int INEQ_FEET = 22;
+
+    /// \name Matrices defining the evolution
+    /// \{
+    struct dynamics_t
+    {
+      /// \brief Matrix of the quadratic part
+      boost_ublas::matrix<double> U;
+      boost_ublas::matrix<double> UT;
+
+      /// \brief Matrix of the linear part
+      boost_ublas::matrix<double> S;
+
+      int type;
+    };
 
     /// \name QP elements that are objective independent
     /// \{
@@ -65,10 +87,18 @@ namespace PatternGeneratorJRL
       /// \brief TrunkState
       trunk_t Trunk;
 
-      /// \brief Selection matrix for the previewed and current feet positions.
-      MAL_MATRIX(V,double);
-      MAL_MATRIX(VT,double);
-      MAL_VECTOR(Vc,double);
+      /// \name Selection matrices
+      /// \{
+      /// \brief Selection matrix for the previewed feet positions
+      boost_ublas::matrix<double> V;
+      boost_ublas::matrix<double> VT;
+      /// \brief Selection matrix for the current feet position
+      boost_ublas::vector<double> Vc;
+      /// \brief Selection matrix for the current foot position
+      boost_ublas::vector<double> Vc_f;
+      /// \brief Selection matrix for relative feet positions
+      boost_ublas::matrix<double> V_f;
+      /// \}
 
       /// \brief Current support foot position
       supportfoot_t SupportFoot;
@@ -83,20 +113,18 @@ namespace PatternGeneratorJRL
     struct objective_variant_s
     {
       double weight;
-      /// \brief Matrix of the quadratic part
-      MAL_MATRIX(U,double);
-      MAL_MATRIX(UT,double);
 
-      /// \brief Matrix of the linear part
-      MAL_MATRIX(S,double);
+      struct dynamics_t * dyn;
 
       /// \brief Minimization objective
       int type;
 
+      std::ostream& print (std::ostream& o) const;
+      void dump(const char * filename) const;
+
     };
     typedef objective_variant_s objective_variant_t;
     /// \}
-
 
     //
     //Public methods
@@ -115,8 +143,16 @@ namespace PatternGeneratorJRL
     { return m_StateMatrices; };
 
     /// \brief Accessors to the objective dependent matrices
-    objective_variant_t const & Objective( const int aObjectiveType ) const;
-    objective_variant_t & Objective( const int aObjectiveType );
+    objective_variant_t const & Objective( const int type ) const;
+    objective_variant_t & Objective( const int type );
+
+    /// \brief Accessors to the dynamics
+    dynamics_t const & Dynamics( const int type ) const;
+    dynamics_t & Dynamics( const int type );
+
+    /// \brief Accessors to the inequalities
+    linear_inequality_t const & Inequalities( const int type ) const;
+    linear_inequality_t & Inequalities( const int type );
 
     /// \brief Accessors to the Center of Mass
     inline com_t const & CoM() const
@@ -150,12 +186,25 @@ namespace PatternGeneratorJRL
   private:
 
     objective_variant_t
-      m_MeanVelocity,
+    m_MeanVelocity,
       m_InstantVelocity,
       m_COPCentering,
-      m_Jerk;
+      m_JerkMin;
 
-    state_variant_t m_StateMatrices;
+    state_variant_t
+    m_StateMatrices;
+
+    dynamics_t
+    m_Position,
+      m_Velocity,
+      m_Acceleration,
+      m_Jerk,
+      m_CoP;
+
+    linear_inequality_t
+    m_IneqCoP,
+      m_IneqCoM,
+      m_IneqFeet;
 
     /// \brief Cholesky decomposition of the initial objective function $Q$
     MAL_MATRIX(m_LQ,double);
@@ -172,6 +221,8 @@ namespace PatternGeneratorJRL
     MAL_MATRIX(m_OptD,double);
 
   };
+
+  std::ostream& operator<< (std::ostream& o, const IntermedQPMat::objective_variant_s& r);
 }
 
 
