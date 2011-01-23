9cf159bf597d812af711f3de56d1cf64e1edae9c
Andrei <andrei.herdt@inrialpes.fr>
2011-01-23 21:11:58 +0100

    Encapsulation, restructuring, documentation
    - add interpolate_trunk_orientation
    - hide implementation
    - move some members to locally declarated variables
    - remove dumping procedures
    - add accessors
    - simplify constructor call
    - add documentation

--- src/ZMPRefTrajectoryGeneration/OrientationsPreview.h
+++ src/ZMPRefTrajectoryGeneration/OrientationsPreview.h
@@ -33,110 +33,175 @@
 
 
 
-/*! STL includes */
 #include <deque>
 
-/*! Framework includes */
 #include <PreviewControl/SupportFSM.h>
-
-/*! Framework includes */
 #include <jrl/walkgen/pgtypes.hh>
 #include <abstract-robot-dynamics/joint.hh>
 
 namespace PatternGeneratorJRL
 {
+  /// \brief The acceleration phase is fixed
   class OrientationsPreview {
+
+    //
+    // Public methods:
+    //
   public:
-    OrientationsPreview(const double & SamplingPeriod,
-			const unsigned int & SamplingsPreviewed, const double & SSPeriod,
-			CjrlJoint *aLeg);
+
+    /// \name Accessors
+    /// \{
+    OrientationsPreview( CjrlJoint *aLeg );
     ~OrientationsPreview();
+    /// \}
 
-    void previewOrientations(const double &Time,
-			     std::deque<double> &PreviewedSupportAngles,
-			     const COMState &TrunkState, COMState &TrunkStateT,
-			     double StepDuration, support_state_t CurrentSupport,
-			     std::deque<FootAbsolutePosition> &LeftFootAbsolutePositions,
-			     std::deque<FootAbsolutePosition> &RightFootAbsolutePositions);
+    /// \brief Preview feet orientations inside the preview window
+    ///
+    /// \param[in] Time
+    /// \param[in] Ref
+    /// \param[in] TrunkState
+    /// \param[in] TrunkStateT
+    /// \param[in] StepDuration
+    /// \param[in] CurrentSupport
+    /// \param[in] LeftFootAbsolutePositions
+    /// \param[in] RightFootAbsolutePositions
+    /// \param[out] PreviewedSupportAngles
+    void preview_orientations(double Time, const reference_t & Ref,
+			     double StepDuration, const support_state_t & CurrentSupport,
+			     std::deque<FootAbsolutePosition> & LeftFootAbsolutePositions,
+			     std::deque<FootAbsolutePosition> & RightFootAbsolutePositions,
+                             std::deque<double> &PreviewedSupportAngles);
 
-    void verifyAccelerationOfHipJoint(const reference_t &Ref,
-				      const COMState &TrunkState, COMState &TrunkStateT,
-				      support_state_t CurrentSupport);
+    /// \brief Interpolate orientation of the trunk
+    ///
+    /// \param[in] time
+    /// \param[in] CurrentIndex
+    /// \param[in] TrunkState
+    /// \param[in] TrunkStateT
+    /// \param[in] NewSamplingPeriod
+    /// \param[in] CurrentSupport
+    /// \param[out] FinalCOMTraj_deq
+    void interpolate_trunk_orientation(double time, int CurrentIndex,
+        double NewSamplingPeriod,
+        const support_state_t & CurrentSupport,
+        std::deque<COMState> & FinalCOMTraj_deq);
 
+    /// \name Accessors
+    /// \{
+    inline COMState const & CurrentTrunkState() const
+    { return TrunkState_; };
+    inline void CurrentTrunkState(const COMState & TrunkState)
+    { TrunkState_ = TrunkState; };
+    inline double SSLength() const
+    { return SSPeriod_; };
+    inline void SSLength( double SSPeriod)
+    { SSPeriod_ = SSPeriod; };
+    inline double SamplingPeriod() const
+    { return m_T; };
+    inline void SamplingPeriod( double SamplingPeriod)
+    { m_T = SamplingPeriod; };
+    inline double NbSamplingsPreviewed() const
+    { return m_N; };
+    inline void NbSamplingsPreviewed( double SamplingsPreviewed)
+    { m_N = SamplingsPreviewed; };
+    /// \}
+
+    //
+    // Private methods:
+    //
   private:
-    /*! Angular limitations of the hip joints*/
-    double m_lLimitLeftHipYaw, m_uLimitLeftHipYaw, m_lLimitRightHipYaw, m_uLimitRightHipYaw;
 
-    /*! Maximal acceleration of a hip joint*/
-    double m_uaLimitHipYaw;
+    /// \brief Verify and eventually reduce the acceleration of the hip joint
+    ///
+    /// \param[in] Ref Reference
+    /// \param[in] TrunkState Current trunk state
+    /// \param[out] TrunkStateT Trunk state at the next future sample
+    /// \param [in] CurrentSupport Current support state
+    void verify_acceleration_hip_joint(const reference_t & Ref,
+                                      const support_state_t & CurrentSupport);
 
-    /*! Upper crossing angle limit between the feet*/
-    double m_uLimitFeet;
+    /// \brief Verify velocity of hip joint
+    ///
+    /// \param[in] Time
+    /// \param[in] TrunkStateT
+    /// \param[in] PreviewedSupportFoot
+    /// \param[in] PreviewedSupportAngle
+    /// \param[in] StepNumber
+    /// \param[in] CurrentSupport
+    /// \param[in] CurrentRightFootAngle
+    /// \param[in] CurrentLeftFootAngle
+    /// \param[in] CurrentLeftFootVelocity
+    /// \param[in] CurrentRightFootVelocity
+    void verify_velocity_hip_joint(double Time, COMState &TrunkStateT,
+                                  double PreviewedSupportFoot,
+                                  double PreviewedSupportAngle, unsigned StepNumber,
+                                  support_state_t CurrentSupport,
+                                  double CurrentRightFootAngle, double CurrentLeftFootAngle,
+                                  double CurrentLeftFootVelocity,
+                                  double CurrentRightFootVelocity);
 
-    /*! Maximal velocity of a foot*/
-    double m_uvLimitFoot;
+    /// \brief Verify angle of hip joint
+    ///
+    /// \param[in] CurrentSupport
+    /// \param[in] PreviewedTrunkAngleEnd
+    /// \param[in] TrunkState
+    /// \param[in] TrunkStateT
+    /// \param[in] CurrentSupportAngle
+    /// \param[in] StepNumber
+    /// \return AngleOK
+    bool verify_angle_hip_joint(support_state_t CurrentSupport,
+                               double PreviewedTrunkAngleEnd,
+                               const COMState &TrunkState, COMState &TrunkStateT,
+                               double CurrentSupportFootAngle,
+                               unsigned StepNumber);
 
-    /*! Single-support duration*/
-    double m_SSPeriod;
-
-    /*! Number of sampling in a preview window*/
-    double m_N;
-
-    /*! Time between two samplings*/
-    double m_T;
-
-    //Polynomial coefficients
-    double m_a, m_b, m_c, m_d, m_e;
-
-    //Rotation sense of the trunks angular velocity and acceleration
-    double m_signRotVelTrunk, m_signRotAccTrunk;
-
-    //Time period between now and the end of the support phase
-    double m_SupportTimePassed;
-
-    bool m_TrunkVelOK, m_TrunkAngleOK;
-
-    /*! In case of double support the next support angle is fixed*/
-    unsigned int m_FirstPreviewedFoot;
-
-    /*! Trunkangle at the end of the current support phase*/
-    double m_PreviewedTrunkAngleEnd;
-
-    const static double M_EPS;
-
-    /*! The angles of the support and non-support foot*/
-    double m_PreviewedMovingAngle, m_PreviewedSupportAngle;
-
-    double m_PreviewedRightFootAngle, m_PreviewedLeftFootAngle;
-
-    double m_PreviousSupportAngle;
-
-    double m_CurrentSupportAngle;
-
-    double m_MeanFootVelDifference;
-
-    /*! Which foot is on the ground in the preview period*/
-    int m_PreviewedSupportFoot;
-
-
-    unsigned int m_FullDebug;
-
-
-    bool verifyAngleOfHipJoint(support_state_t CurrentSupport,
-			       const COMState &TrunkState, COMState &TrunkStateT,
-			       double CurrentSupportFootAngle,
-			       unsigned int StepNumber);
-
-    void verifyVelocityOfHipJoint(const double &Time, COMState &TrunkStateT,
-				  const double &PreviewedSupportFoot, const unsigned int &StepNumber,
-				  support_state_t CurrentSupport,
-				  const double &CurrentRightFootAngle, const double &CurrentLeftFootAngle,
-				  const double &CurrentLeftFootVelocity,
-				  const double &CurrentRightFootVelocity);
-
+    /// \brief Fourth order polynomial trajectory
     double f(double a,double b,double c,double d,double x);
 
+    /// \brief Fourth order polynomial trajectory derivative
     double df(double a,double b,double c,double d,double x);
+
+    //
+    // Private members:
+    //
+  private:
+
+    /// \brief Angular limitations of the hip joints
+    double m_lLimitLeftHipYaw, m_uLimitLeftHipYaw, m_lLimitRightHipYaw, m_uLimitRightHipYaw;
+
+    /// \brief Maximal acceleration of a hip joint
+    double m_uaLimitHipYaw;
+
+    /// \brief Upper crossing angle limit between the feet
+    double m_uLimitFeet;
+
+    /// \brief Maximal velocity of a foot
+    double m_uvLimitFoot;
+
+    /// \brief Single-support duration
+    double SSPeriod_;
+
+    /// \brief Number of sampling in a preview window
+    double m_N;
+
+    /// \brief Time between two samplings
+    double m_T;
+
+    /// \brief Rotation sense of the trunks angular velocity and acceleration
+    double m_signRotVelTrunk, m_signRotAccTrunk;
+
+    /// \brief
+    double m_SupportTimePassed;
+
+    /// \brief
+    const static double M_EPS;
+
+    /// \brief Current trunk state
+    COMState TrunkState_;
+    /// \brief State of the trunk at the first previewed sampling
+    COMState TrunkStateT_;
+
+
 
   };
 }
