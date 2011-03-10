16d075006ec34c905b777f2eae89f869ea4c8467
olivier-stasse <olivier.stasse@aist.go.jp>
2011-03-11 05:07:24 +0900

    Revert "Minor changes"
    This reverts commit f7dfa93b367aeb71db0e0bd10859c221e6d21275.

--- src/PreviewControl/LinearizedInvertedPendulum2D.h
+++ src/PreviewControl/LinearizedInvertedPendulum2D.h
@@ -110,7 +110,6 @@ namespace PatternGeneratorJRL
     /*! \brief State of the LIPM at the \f$k\f$ eme iteration
       \f$ x_k = [ c_x \dot{c}_x \ddot{c}_x c_y \dot{c}_y \ddot{c}_y\f$ */
     MAL_VECTOR(m_xk,double);
-
     com_t m_CoM;
 
     /* ! \brief Vector of ZMP  */
@@ -142,12 +141,10 @@ namespace PatternGeneratorJRL
     void SetRobotControlPeriod(const double &);
 
     /// \brief Accessor
-    inline const com_t operator ()() const
-    {return m_CoM;};
+    com_t operator ()() const;
 
     /// \brief Accessor
-    inline void operator ()( com_t CoM )
-    {m_CoM = CoM;};
+    void operator ()( com_t CoM );
 
     /*! Get state. */
     void GetState(MAL_VECTOR_TYPE(double) &lxk);
