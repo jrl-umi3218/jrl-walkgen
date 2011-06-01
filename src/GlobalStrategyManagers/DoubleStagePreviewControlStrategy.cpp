32f4412be51918b527e004e32793e7258aeca821
olivier-stasse <olivier.stasse@aist.go.jp>
2011-06-02 05:09:38 +0900

    Fix package name.

--- src/GlobalStrategyManagers/DoubleStagePreviewControlStrategy.cpp
+++ src/GlobalStrategyManagers/DoubleStagePreviewControlStrategy.cpp
@@ -7,18 +7,18 @@
  *
  * JRL, CNRS/AIST
  *
- * This file is part of walkGenJrl.
- * walkGenJrl is free software: you can redistribute it and/or modify
+ * This file is part of jrl-walkgen.
+ * jrl-walkgen is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * walkGenJrl is distributed in the hope that it will be useful,
+ * jrl-walkgen is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Lesser Public License for more details.
  * You should have received a copy of the GNU Lesser General Public License
- * along with walkGenJrl.  If not, see <http://www.gnu.org/licenses/>.
+ * along with jrl-walkgen.  If not, see <http://www.gnu.org/licenses/>.
  *
  *  Research carried out within the scope of the 
  *  Joint Japanese-French Robotics Laboratory (JRL)
