jrl-umi3218/jrl-walkgen

commit 846be4ab58282d87ef679a59ad7aa05316c464e6

Correct OrientationsPreview: the order of left/right feet is no longer hard coded.

src/ZMPRefTrajectoryGeneration/OrientationsPreview.cpp
@@ -37,27 +37,35 @@ using namespace std;
 
 const double OrientationsPreview::EPS_ = 0.00000001;
 
-OrientationsPreview::OrientationsPreview( CjrlJoint *aRootJoint)
+OrientationsPreview::OrientationsPreview( CjrlHumanoidDynamicRobot *aHS)
 {
+  CjrlJoint * waist     = aHS->waist();
 
-  lLimitLeftHipYaw_ = aRootJoint->childJoint(1)->lowerBound(0);//-30.0/180.0*M_PI;
-  uLimitLeftHipYaw_  = aRootJoint->childJoint(1)->upperBound(0);//45.0/180.0*M_PI;
+  // left hip
+  CjrlJoint * leftFoot  = aHS->leftFoot()->associatedAnkle();
+  CjrlJoint * leftHip   = aHS->jointsBetween(*waist, *leftFoot)[0];
+
+  lLimitLeftHipYaw_  = leftHip->lowerBound(0);//-30.0/180.0*M_PI;
+  uLimitLeftHipYaw_  = leftHip->upperBound(0);//45.0/180.0*M_PI;
   if (lLimitLeftHipYaw_==  uLimitLeftHipYaw_)
     {
       lLimitLeftHipYaw_ = -30.0/180.0*M_PI;
       uLimitLeftHipYaw_ = 45.0/180.0*M_PI;
     }
 
-  lLimitRightHipYaw_ = aRootJoint->childJoint(0)->lowerBound(0);//-45.0/180.0*M_PI;
-  uLimitRightHipYaw_ = aRootJoint->childJoint(0)->upperBound(0);//30.0/180.0*M_PI;
+  // right hip
+  CjrlJoint * rightFoot = aHS->rightFoot()->associatedAnkle();
+  CjrlJoint * rightHip  = aHS->jointsBetween(*waist, *rightFoot)[0];
+
+  lLimitRightHipYaw_ = rightHip->lowerBound(0);//-45.0/180.0*M_PI;
+  uLimitRightHipYaw_ = rightHip->upperBound(0);//30.0/180.0*M_PI;
   if (lLimitRightHipYaw_==  uLimitRightHipYaw_)
     {
       lLimitRightHipYaw_ = -30.0/180.0*M_PI;
       uLimitRightHipYaw_ = 45.0/180.0*M_PI;
     }
 
-
-  uvLimitFoot_ = fabs(aRootJoint->childJoint(0)->upperVelocityBound(0));
+  uvLimitFoot_ = fabs(leftHip->upperVelocityBound(0));
 
   //Acceleration limit not given by HRP2JRLmain.wrl
   uaLimitHipYaw_ = 0.1;

src/ZMPRefTrajectoryGeneration/OrientationsPreview.hh
@@ -37,7 +37,7 @@
 
 #include <privatepgtypes.hh>
 #include <jrl/walkgen/pgtypes.hh>
-#include <abstract-robot-dynamics/joint.hh>
+#include <abstract-robot-dynamics/humanoid-dynamic-robot.hh>
 
 namespace PatternGeneratorJRL
 {
@@ -51,7 +51,7 @@ namespace PatternGeneratorJRL
 
     /// \name Accessors
     /// \{
-    OrientationsPreview( CjrlJoint *aLeg );
+    OrientationsPreview( CjrlHumanoidDynamicRobot *aHS );
     ~OrientationsPreview();
     /// \}
 

src/ZMPRefTrajectoryGeneration/ZMPVelocityReferencedQP.cpp
@@ -80,7 +80,7 @@ ZMPVelocityReferencedQP::ZMPVelocityReferencedQP(SimplePluginManager *SPM,
   SupportFSM_->SamplingPeriod( QP_T_ );
 
   // Create and initialize preview of orientations
-  OrientPrw_ = new OrientationsPreview( aHS->rootJoint() );
+  OrientPrw_ = new OrientationsPreview( aHS );
   OrientPrw_->SamplingPeriod( QP_T_ );
   OrientPrw_->NbSamplingsPreviewed( QP_N_ );
   OrientPrw_->SSLength( SupportFSM_->StepPeriod() );

commit d18df6aa16baf7cb845576932a5097017d04f97f

Correct bug introduced in 846be4ab58282d87.
The symptoms only appear when walking and turning.
After a turn on the stop, the robot may start turning
in the other direction (even when asking for a straight walk).

src/ZMPRefTrajectoryGeneration/OrientationsPreview.cpp
@@ -43,7 +43,7 @@ OrientationsPreview::OrientationsPreview( CjrlHumanoidDynamicRobot *aHS)
 
   // left hip
   CjrlJoint * leftFoot  = aHS->leftFoot()->associatedAnkle();
-  CjrlJoint * leftHip   = aHS->jointsBetween(*waist, *leftFoot)[0];
+  CjrlJoint * leftHip   = aHS->jointsBetween(*waist, *leftFoot)[1];
 
   lLimitLeftHipYaw_  = leftHip->lowerBound(0);//-30.0/180.0*M_PI;
   uLimitLeftHipYaw_  = leftHip->upperBound(0);//45.0/180.0*M_PI;
@@ -55,7 +55,7 @@ OrientationsPreview::OrientationsPreview( CjrlHumanoidDynamicRobot *aHS)
 
   // right hip
   CjrlJoint * rightFoot = aHS->rightFoot()->associatedAnkle();
-  CjrlJoint * rightHip  = aHS->jointsBetween(*waist, *rightFoot)[0];
+  CjrlJoint * rightHip  = aHS->jointsBetween(*waist, *rightFoot)[1];
 
   lLimitRightHipYaw_ = rightHip->lowerBound(0);//-45.0/180.0*M_PI;
   uLimitRightHipYaw_ = rightHip->upperBound(0);//30.0/180.0*M_PI;